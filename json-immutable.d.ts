1263ae840248aee06607eedd88716b1b3c2ea367
liuxiong <liuxiong332@163.com>
2019-07-29 11:26:26 +0800

    Add types

---
+++ json-immutable.d.ts
@@ -0,0 +1,21 @@
+// Type definitions for [json-immutable]
+// Project: [json-immutable]
+// Definitions by: liuxiong
+
+/*~ This is the module template file. You should rename it to index.d.ts
+ *~ and place it in a folder with the same name as the module.
+ *~ For example, if you were writing a file for "super-greeter", this
+ *~ file should be 'super-greeter/index.d.ts'
+ */
+
+/*~ If this module is a UMD module that exposes a global variable 'myLib' when
+ *~ loaded outside a module loader environment, declare that global here.
+ *~ Otherwise, delete this declaration.
+ */
+// export as namespace myLib;
+
+/*~ If this module has methods, declare them as functions like so.
+ */
+export function serialize(data: any, options?: { pretty: boolean }): string;
+
+export function deserialize(json: string, options?: any): any;
